9c965474f8bec6c3df4883e176c0f4501a2077b6
Maxim Makovskiy <makovskiyms@gmail.com>
2022-03-16 22:01:33 +0600

    Fix quicksort error

--- ch05/ex_05_07.c
+++ ch05/ex_05_07.c
@@ -84,7 +84,7 @@ void qsort(char *v[], int left, int right) {
   }
   swap(v, left, last);
   qsort(v, left, last - 1);
-  qsort(v, left + 1, right);
+  qsort(v, last + 1, right);
 }
 
 void swap(char *v[], int i, int j) {
